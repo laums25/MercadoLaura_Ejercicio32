1b0bf8ab47a421c87b273a0ffde6264608ad926c
laums25 <56890898+laums25@users.noreply.github.com>
2019-11-28 13:43:17 -0500

    final

--- Ejercicio32.py
+++ Ejercicio32.py
@@ -8,6 +8,13 @@ plt.subplot(1,2,2)
 data = np.loadtxt("Ejer.dat")
 
 plt.plot(data[:,0], data[:,1])
+plt.plot(data[:,0], data[:,2])
+plt.plot(data[:,0], data[:,3])
+plt.plot(data[:,0], data[:,4])
+plt.plot(data[:,0], data[:,5])
+plt.plot(data[:,0], data[:,6])
+plt.plot(data[:,0], data[:,7])
+
 
 plt.subplot(1,2,1)
 plt.imshow(data.T)            
